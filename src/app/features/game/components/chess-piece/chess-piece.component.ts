5c206a4b6fafad8f78b53efdd09d7bf026991666
honam <75104291+honambsn@users.noreply.github.com>
2025-06-21 17:03:23 +0700

    update: period 3, miss pieces images, add later

--- src/app/features/game/components/chess-piece/chess-piece.component.ts
+++ src/app/features/game/components/chess-piece/chess-piece.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Piece } from '../../../../core/models/piece.model';
 
 @Component({
   selector: 'app-chess-piece',
   standalone: true,
   templateUrl: './chess-piece.component.html',
   styleUrl: './chess-piece.component.scss',
-  imports: [],
+  //imports: [],
 })
 export class ChessPieceComponent {
+  @Input() piece!: Piece;
 
+  get pieceString(): string {
+const type = this.piece.type.toString().toLowerCase();  // ensure string
+  const player = this.piece.player.toString().toLowerCase();
+  
+    //return this.piece ? `${this.piece.type}${this.piece.color}` : '';
+    return `assets/images/pieces/${type}-${player}.png`;
+
+  }
 }
--- src/app/features/game/components/game-board/game-board.component.ts
+++ src/app/features/game/components/game-board/game-board.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Board } from '../../../../core/models/board.model';
+import { Piece } from '../../../../core/models/piece.model';
+import { ChessPieceComponent } from './../chess-piece/chess-piece.component';
+
+
 
 @Component({
   selector: 'app-game-board',
   standalone: true,
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.scss',
-  imports: [],
+  imports: [ChessPieceComponent],
 })
-export class GameBoardComponent {
+export class GameBoardComponent implements OnInit {
+  board = new Board();
+  
+  rows = Array.from({length: 10}, (_, i) => i);
+  columns = Array.from({length: 9}, (_, i) => i);
 
+  getPieceAt(row: number, col: number): Piece | null {
+    return this.board.pieces.find(p => p.position.row === row && p.position.col === col) || null;
+  }
+
+  ngOnInit(): void {
+    // Initialize the game board with pieces
+    //this.board.initPieces();
+    
+    // Log the pieces to the console for debugging
+    console.log('Game Board Pieces:', this.board.pieces);
+  }
 }
